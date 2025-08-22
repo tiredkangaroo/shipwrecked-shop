822158c92775637312ce45605af36c421af3e6e9
Ajitesh Kumar <ajinest6@gmail.com>
2025-08-22 13:20:28 -0400

    build frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+  base: "/shipwrecked-shop/",
   plugins: [react(), tailwindcss()],
 });
